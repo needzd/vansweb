f1cc82bd83db00b1c0885ac901ca11f7a75573d9
needzd <1572009273@qq.com>
2019-03-30 11:30:41 +0800

    项目完成

--- js/shopcar.js
+++ js/shopcar.js
@@ -36,9 +36,9 @@ function shoe(str){
                 <input class="count_text" type="text" value="${(str[i].goodsCount)}" />
                 <p class="count_jia">+</p>
             </span>
-            <span class="count_dj">￥${str[i].goodsPrice}.00</span>
+            <span class="count_dj">￥<i class="goodsPrice">${str[i].goodsPrice}.00</i></span>
             <span class="count_yh">￥0</span>
-            <span class="count_zj">￥595.00</span>
+            <span class="count_zj">￥<i class="zj_price">0</i></span>
             <span class="yiruchu"><a href="#">移入收藏夹</a> /<a href="#">移除</a></span>
         </div>
         `  
@@ -51,32 +51,80 @@ function shoe(str){
 }  
 
 
-function jia_jian(){
+function jia_jian(){  
+    let zong_count = $(".hd_my_show").html();
     $(".count_jia").each(function(){
         $(this).click(function(){
-            let c_jia = $(this).prev().val();
-            let zong_count = $(".hd_my_show").html();
+            let c_jia = Number($(this).prev().val());
+            let price = parseFloat($(this).parent().next().children().html());
+            let money = c_jia * price;
             c_jia++;
             zong_count++;
             $(this).prev().val(c_jia);
+            money = c_jia * price;
             $(".hd_my_show").html(zong_count);
+            $(this).parent().next().next().next().children().html(money);
         });
     });
 
     $(".count_jian").each(function(){
         $(this).click(function(){
             let c_jian = $(this).next().val();
-            let zong_count = $(".hd_my_show").html();
+            let price = parseFloat($(this).parent().next().children().html());
+            let money = c_jian * price;
             if(c_jian==1){
-                // c_jian==1;
                 return;
             }
             c_jian--;
             zong_count--;
             // $.get("php/updateGoodsCount.php",xiugai_count,"json")
+            money = c_jian * price;
             $(this).next().val(c_jian);
             $(".hd_my_show").html(zong_count);
+            $(this).parent().next().next().next().children().html(money);
         });
     });
 }
+
+
+
+// function jia_jian(){  
+//     $(".count_jia").each(function(){
+//         $(this).click(function(){
+//             let c_jia = Number($(this).prev().val());
+//             let zong_count = $(".hd_my_show").html();
+//             let price = parseFloat($(".goodsPrice").html());
+//             let money = c_jia * price;
+//             c_jia++;
+//             zong_count++;
+//             $(this).prev().val(c_jia);
+//             money = c_jia * price;
+//             $(".hd_my_show").html(zong_count);
+//             $(".zj_price").html(money);
+//         });
+//     });
+
+//     $(".count_jian").each(function(){
+//         $(this).click(function(){
+//             let c_jian = $(this).next().val();
+//             let zong_count = $(".hd_my_show").html();
+//             let price = parseFloat($(".goodsPrice").html());
+//             let money = c_jian * price;
+//             if(c_jian==1){
+//                 return;
+//             }
+//             c_jian--;
+//             zong_count--;
+//             // $.get("php/updateGoodsCount.php",xiugai_count,"json")
+//             money = c_jian * price;
+//             $(this).next().val(c_jian);
+//             $(".hd_my_show").html(zong_count);
+//             $(".zj_price").html(money);
+//         });
+//     });
+// }
+
+
+
+
 
